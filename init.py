5d76170be80f12b6928c0c61e2670aeb35e8cbdc
Wallace Medeiros Dias <wallacemedeiros.d@gmail.com>
2024-05-24 13:15:39 -0300

    🚧 Em progresso

--- init.py
+++ init.py
@@ -20,8 +20,23 @@ while True:
     opc = int(input("Opção: "))
     if opc == 1:
         print("Status".center(50))
+        print(player)
+        print(itens)
         print('='*50)
         
+    if opc == 3:
+        print("[1] - Comprar \n[2] - Vender")
+        opc = int(input("Opção: "))
+        if opc == 1:
+            cont = 0
+            for index, c in enumerate(loja, start = 1):
+                print(f'{index}: {c}')
+                cont +=1
+                if cont == len(loja):
+                    buy = int(input("Buy: "))
+                    if buy == index:
+                        itens.append(c[index])
+                        loja.remove(c[index])
     
     if opc == 5:
         print("Saindo . . .")
